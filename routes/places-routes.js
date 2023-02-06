ab78735fbfc80376a1ae4de0026b72d7cef186ee
FadlyMuhammad10 <davit9747@gmail.com>
2023-02-06 12:52:53 +0700

    places routes

---
+++ routes/places-routes.js
@@ -0,0 +1,34 @@
+const express = require("express");
+
+const router = express.Router();
+
+const DUMMY_PLACES = [
+  {
+    id: "p1",
+    title: "Empire State Building",
+    description: "One of the famous",
+    location: {
+      lat: 40.7484405,
+      lng: -73.9856644,
+    },
+    address: "New York",
+    creator: "u1",
+  },
+];
+
+router.get("/:pid", (req, res, next) => {
+  const placeId = req.params.pid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.id === placeId;
+  });
+  res.json(place);
+});
+router.get("/user/:uid", (req, res, next) => {
+  const userId = req.params.uid;
+  const place = DUMMY_PLACES.find((p) => {
+    return p.creator === userId;
+  });
+  res.json(place);
+});
+
+module.exports = router;
